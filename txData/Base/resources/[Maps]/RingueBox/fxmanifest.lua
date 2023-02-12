7ae05bb027cab5b1795add4a88a0c9ef79b8aba1
António Matos <antonio.martim.matos@gmail.com>
2023-02-12 23:51:04 +0000

    Carros/Mapas

---
+++ txData/Base/resources/[Maps]/RingueBox/fxmanifest.lua
@@ -0,0 +1,5 @@
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
+
+client_script "@InfinityAC/src/client/src_c_03.lua"
---
+++ txData/Base/resources/[Maps]/cfx-gabz-catcafe/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+description 'CatCafe'
+version '5.0.0'
+
+this_is_a_map 'yes'
+
+client_script "@InfinityAC/src/client/src_c_03.lua"
